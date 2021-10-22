ef2b4d282777d5e521351ea291898cbf157a024a
Rakibul615 <rakibul01072019@gmail.com>
2021-10-23 00:45:20 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCyGnzgzPVSCX1mWupOJZPcCBlV4kuH9mw",
+  authDomain: "health-care-8c750.firebaseapp.com",
+  projectId: "health-care-8c750",
+  storageBucket: "health-care-8c750.appspot.com",
+  messagingSenderId: "259295434174",
+  appId: "1:259295434174:web:44d0c8b8a2765e2d9555ef"
+};
+export default firebaseConfig;
